cbd4b035cd68d797e2d483e38c3dcb0f655767bc
Ramon <ramoncasadofranco@hotmail.es>
2024-11-05 17:50:17 +0100

    Patron observer segundo prototipo

--- src/observer/Main.java
+++ src/observer/Main.java
@@ -9,9 +9,11 @@ public class Main {
 		Covid19Pacient p1=new Covid19Pacient("aitor", 35);
 		new PacientObserverGUI (p1);
 		new PacientSymptomGUI (p1);
+		new PacientThermometerGUI(p1);
 		Covid19Pacient p2=new Covid19Pacient("ramon", 28);
 		new PacientObserverGUI (p2);
 		new PacientSymptomGUI (p2);
+		new PacientThermometerGUI(p2);
 	}
 
 
